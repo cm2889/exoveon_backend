695aad80d6f63b26e2290450ed6e0b1c15b487ea
ASIF <f.asif.official@gmail.com>
2025-11-14 19:35:01 +0600

    feat: Add 'https://orbitx.uk' to CORS_ALLOWED_ORIGINS and CSRF_TRUSTED_ORIGINS

--- orbitx/settings.py
+++ orbitx/settings.py
@@ -75,6 +75,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://orbitx.uk',
 ]
 CORS_ALLOW_CREDENTIALS = True  
 
@@ -94,6 +95,8 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'https://orbitx.uk', 
+
 ]
 
 ROOT_URLCONF = "orbitx.urls"
