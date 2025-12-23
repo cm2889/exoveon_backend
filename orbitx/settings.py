c61acba72dfe10ee8ef395b2dbf36708dff8434f
ASIF <f.asif.official@gmail.com>
2025-12-23 21:33:10 +0600

    fix: Add http://localhost:4000 to CORS_ALLOWED_ORIGINS and CSRF_TRUSTED_ORIGINS

--- orbitx/settings.py
+++ orbitx/settings.py
@@ -80,6 +80,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://localhost:4000',
     'http://127.0.0.1:3000',
     'http://localhost:8080',
     'http://127.0.0.1:8080',
@@ -132,6 +133,7 @@ MIDDLEWARE = [
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    "http://localhost:4000", 
     'https://orbitx.uk', 
     "https://www.orbitx.uk",
     'http://orbitx.uk', 
